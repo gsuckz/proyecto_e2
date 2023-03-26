d1278a2a57e997a46be4ec094da98f8e9f79ac8e
Jesus Avila <jesus.avila.05@gmail.com>
2023-03-26 12:49:53 -0300

    xd

--- generador_caracteres.vhd
+++ generador_caracteres.vhd
@@ -65,7 +65,7 @@ architecture solucion of generador_caracteres is
     red   <= '1' when color(0) = '1' and px_visible = '1' else '0';
     green <= '1' when color(1) = '1' and px_visible = '1' else '0';
     blue  <= '1' when color(2) = '1' and px_visible = '1' else '0';
-    color_d <= "111" when color = "000"
+    color_d <= "111" when color = "000" else
                 std_logic_vector(unsigned(color)+1) when (mas = '1' and ajuste = "1010") else
                std_logic_vector(unsigned(color)+1) when (mas = '1' and ajuste = "1010") else
                color;
